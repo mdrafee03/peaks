7185cb42a070eeb39b5993fb91183fcd29391d94
Rafee <mdrafee03@gmail.com>
2022-03-25 18:57:12 +0000

    feat: add theme and themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import Topbar from 'components/Topbar/Topbar';
-import Home from 'pages/Home';
+import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Topbar from './components/Topbar/Topbar';
+
+const theme = {
+  colors: {
+    primary: 'rgba(9, 53, 123, 1)',
+  },
+};
 
 const App = (): JSX.Element => (
-  <BrowserRouter>
-    <Topbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 export default App;
