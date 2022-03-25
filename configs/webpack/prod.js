9a3b34ebc49b919ad0eef444b1868489f3ed1a45
Rafee <mdrafee03@gmail.com>
2022-03-26 05:22:53 +0600

    fix: resolve webpack typescript error for dev

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,19 +1,17 @@
 // production config
 const { merge } = require('webpack-merge');
 const { resolve } = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const commonConfig = require('./common');
 
 module.exports = merge(commonConfig, {
-  context: resolve(__dirname, '../..'),
   mode: 'production',
-  entry: './index.tsx',
+  entry: 'src/index.tsx',
   output: {
     filename: 'js/bundle.[contenthash].min.js',
     path: resolve(__dirname, '../../dist'),
     publicPath: '/',
   },
   devtool: 'source-map',
-  plugins: [new ForkTsCheckerWebpackPlugin()],
+  plugins: [],
 });
