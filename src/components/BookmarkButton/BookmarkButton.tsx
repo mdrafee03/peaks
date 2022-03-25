6af753a324fd271df3c8342445c02bc713da250a
Rafee <mdrafee03@gmail.com>
2022-03-25 19:04:34 +0000

    feat: create reusable bookmark button

---
+++ src/components/BookmarkButton/BookmarkButton.tsx
@@ -0,0 +1,31 @@
+import { css } from '@emotion/react';
+import BookMarkIcon from '../../assets/icons/bookmark.svg';
+import Button from '../Button/Button';
+
+const styles = css({
+  background: 'rgba(9, 53, 123, 1)',
+  color: '#fff',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  border: 'none',
+  borderRadius: '4px',
+  fontSize: '13px',
+  padding: '5px 12px 4px 12px',
+  cursor: 'pointer',
+});
+
+interface Props {
+  text: string;
+  onClick: () => void;
+}
+
+const BookmarkButton = ({ text, onClick }: Props): JSX.Element => {
+  return (
+    <Button customStyles={styles} type="button" onClick={onClick}>
+      <img src={BookMarkIcon} alt="bookmark" />
+      <span>{text.toUpperCase()}</span>
+    </Button>
+  );
+};
+export default BookmarkButton;
