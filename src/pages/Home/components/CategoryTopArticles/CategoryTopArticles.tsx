5f5783a6e7e62d520beb1698af78657a1ccc0106
Rafee <mdrafee03@gmail.com>
2022-03-26 22:28:32 +0000

    feat: reuse cardlist in category top articles comp

--- src/pages/Home/components/CategoryTopArticles/CategoryTopArticles.tsx
+++ src/pages/Home/components/CategoryTopArticles/CategoryTopArticles.tsx
@@ -1,31 +1,19 @@
-import { SerializedStyles } from '@emotion/react';
 import { Link } from 'react-router-dom';
-import Card from 'src/components/Card/Card';
+import CardList from 'src/components/CardList/CardList';
 import { GuardianData } from 'src/interfaces/Guardian.interface';
 
 interface Props {
   data: GuardianData[];
   title: string;
   section: string;
-  styles: SerializedStyles;
 }
-const CategoryTopArticles = ({ data, title, section, styles }: Props): JSX.Element => {
+const CategoryTopArticles = ({ data, title, section }: Props): JSX.Element => {
   return (
     <>
       <Link to={`/category/${section}`}>
         <h1>{title}</h1>
       </Link>
-      <div css={styles}>
-        {data.map((article) => (
-          <Link
-            to={`/article/${encodeURIComponent(article.id)}`}
-            className={`card-wrapper`}
-            key={article.id}
-          >
-            <Card title={article.webTitle} body={article?.fields?.trailText} />
-          </Link>
-        ))}
-      </div>
+      <CardList articles={data} />
     </>
   );
 };
