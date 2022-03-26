d770883269a130ab488554bfd08dd72e157a2e9f
Rafee <mdrafee03@gmail.com>
2022-03-26 22:25:40 +0000

    fix: type casting to prevent ts error

--- src/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -3,7 +3,7 @@ import { useCallback, useRef } from 'react';
 const useInfiniteScroll = (isLoading: boolean, hasMore: boolean, loadMore: () => void) => {
   const observer = useRef<IntersectionObserver>();
   const lastElementRef = useCallback(
-    (node) => {
+    (node: unknown) => {
       if (isLoading) return;
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
@@ -11,7 +11,7 @@ const useInfiniteScroll = (isLoading: boolean, hasMore: boolean, loadMore: () =>
           loadMore();
         }
       });
-      if (node) observer.current.observe(node);
+      if (node) observer.current.observe(node as Element);
     },
     [isLoading, hasMore],
   );
