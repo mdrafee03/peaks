17074b11134107038d86928c8d933f26685377d5
Rafee <mdrafee03@gmail.com>
2022-03-26 22:19:00 +0000

    feat: add bottom line color to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,61 +1,78 @@
 import { css } from '@emotion/react';
 import Logo from 'src/assets/logo.svg';
-const styles = css({
-  width: '100%',
-  height: '100%',
-  position: 'relative',
-  img: { width: '100%', height: '100%', objectFit: 'contain' },
-  '& .defaultImg': {
-    background: 'rgba(9, 53, 123, 0.9)',
+
+const styles = (bottomLineColor?: string) =>
+  css({
     width: '100%',
     height: '100%',
-    textAlign: 'center',
-    img: { width: '60%', height: 'auto', paddingTop: '5rem' },
-  },
-  '& .text-wrapper': {
-    position: 'absolute',
-    bottom: '0',
-    background: 'rgba(9, 53, 123, 0.9)',
-    padding: '10px',
-    width: '100%',
-    boxSizing: 'border-box',
-    header: {
-      fontWeight: 700,
-      lineHeight: '30px',
-      fontSize: '24px',
-      color: '#fff',
+    position: 'relative',
+    '& .image-wrap': {
+      width: '100%',
+      height: 'calc(100% - 3px)',
     },
-    '& p': {
-      fontSize: '14px',
-      color: '#fff',
-      lineHeight: '20px',
-      letterSpacing: '0.1px',
-      margin: '0',
-      padding: '5px 0',
+    img: { width: '100%', height: '100%', objectFit: 'cover' },
+    '& .defaultImg': {
+      background: 'rgba(9, 53, 123, 0.9)',
+      textAlign: 'center',
+      img: { width: '60%', height: 'auto', paddingTop: '5rem' },
     },
-  },
-});
+    '& .text-wrapper': {
+      position: 'absolute',
+      bottom: '3px',
+      background: 'rgba(9, 53, 123, 0.9)',
+      padding: '10px',
+      width: '100%',
+      height: '35%',
+      overflow: 'hidden',
+      boxSizing: 'border-box',
+      header: {
+        fontWeight: 700,
+        lineHeight: '30px',
+        fontSize: '1em',
+        color: '#fff',
+      },
+      '& p': {
+        fontSize: '14px',
+        color: '#fff',
+        lineHeight: '20px',
+        letterSpacing: '0.1px',
+        margin: '0',
+        padding: '5px 0',
+      },
+    },
+    hr: {
+      position: 'relative',
+      margin: 0,
+      height: '3px',
+      background: bottomLineColor ?? '#D32F2F',
+      border: 'none',
+    },
+  });
 
 interface Props {
   imgUrl?: string;
   title: string;
   body?: string;
+  bottomLineColor?: string;
 }
 
-const Card = ({ imgUrl, title, body }: Props): JSX.Element => {
+const Card = ({ imgUrl, title, body, bottomLineColor }: Props): JSX.Element => {
   return (
-    <section css={styles}>
+    <section css={styles(bottomLineColor)}>
       {imgUrl ? (
-        <img src={imgUrl} alt="card" />
+        <div className="image-wrap">
+          <img src={imgUrl} alt="card" />
+        </div>
       ) : (
-        <div className="defaultImg">
+        <div className="image-wrap defaultImg">
           <img src={Logo} alt="default" />
         </div>
       )}
-      <section className="text-wrapper">
+      <article className="text-wrapper">
         <header>{title}</header>
         {body && <p dangerouslySetInnerHTML={{ __html: body }}></p>}
-      </section>
+      </article>
+      <hr></hr>
     </section>
   );
 };
