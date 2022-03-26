a32b0b4825a6f2f08c04f704b8f3cef35c706b17
Rafee <mdrafee03@gmail.com>
2022-03-26 22:41:01 +0000

    fix: reuse cardlist in search result component

--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -1,24 +1,9 @@
-import { css } from '@emotion/react';
 import { useEffect, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
-import Card from 'src/components/Card/Card';
+import { useSearchParams } from 'react-router-dom';
+import CardList from 'src/components/CardList/CardList';
 import ContentHeader from 'src/components/ContentHeader/ContentHeader';
-import Loader from 'src/components/Loader/Loader';
 import useInfiniteScroll from 'src/hooks/useInfiniteScroll/useInfiniteScroll';
 import useSearchQuery from './hooks/useSearchQuery/useSearchQuery';
-
-const styles = css({
-  display: 'flex',
-  alignItems: 'flex-start',
-  flexWrap: 'wrap',
-  '& .card-wrapper': {
-    flex: '1 1 30%',
-    marginRight: '30px',
-    marginBottom: '30px',
-    minWidth: '300px',
-    height: '350px',
-  },
-});
 
 const SearchResult = (): JSX.Element => {
   const { request, articles, setArticles, isLoading, hasMore } = useSearchQuery('news');
@@ -49,19 +34,7 @@ const SearchResult = (): JSX.Element => {
   return (
     <>
       <ContentHeader title="Search Result" onSelect={selectHandler} />
-      <section css={styles}>
-        {articles.map((article, i) => (
-          <Link
-            to={`/article/${encodeURIComponent(article.id)}`}
-            className={`card-wrapper`}
-            key={article.id}
-            ref={articles.length === i + 1 ? lastElementRef : null}
-          >
-            <Card title={article.webTitle} body={article?.fields?.trailText} />
-          </Link>
-        ))}
-        {isLoading && <Loader />}
-      </section>
+      <CardList articles={articles} lastElementRef={lastElementRef} />
     </>
   );
 };
